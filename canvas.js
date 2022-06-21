f3a31ecbe4513ef48bdbffb485910d8ef16a730a
Sam <samworth27@gmail.com>
2022-06-21 10:06:55 +1000

    working on limiting turns

--- canvas.js
+++ canvas.js
@@ -28,8 +28,8 @@ class Particle {
   constructor(position, velocity, size, colour) {
     this.position = position;
     this.velocity = velocity;
-    this.acceleration = { linear: 0, angular: 0 };
-    this.maxVelocity = { linear: 2, angular: 5 };
+    this.acceleration = new Vector2({ x: 0, y: 0 })
+    this.maxVelocity = { linear: 2, angular: 1 };
     this.size = size;
     this.colour = colour;
   }
@@ -38,39 +38,47 @@ class Particle {
     ctx.beginPath();
     ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2, false);
     ctx.fillStyle = this.colour;
-    ctx.strokeStyle = "#000000"
+    ctx.strokeStyle = "#000000";
     ctx.strokeWidth = 1;
     ctx.moveTo(this.position.x, this.position.y);
-    ctx.lineTo(this.position.x, this.position.y+10);
+    ctx.lineTo(this.position.x, this.position.y + 10);
     ctx.fill();
-    
-    
   }
 
-
-  update() {
-    this.colour = "#8C5523";
-
+  avoidEdges() {
     if (this.position.x > canvas.width - buffer) {
       this.colour = "#FF0000";
       // rotate towards center
-      this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      // this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      this.acceleration.x = screenCenter.x - this.position.x;
+      this.acceleration.y = screenCenter.y - this.position.y;
     }
     if (this.position.x < buffer) {
       this.colour = "#FF0000";
       // rotate towards center
-      this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      // this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      this.acceleration.x = screenCenter.x - this.position.x;
+      this.acceleration.y = screenCenter.y - this.position.y;
     }
     if (this.position.y > canvas.height - buffer) {
       this.colour = "#0000FF";
       // rotate towards center
-      this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      // this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      this.acceleration.x = screenCenter.x - this.position.x;
+      this.acceleration.y = screenCenter.y - this.position.y;
     }
     if (this.position.y < buffer) {
       this.colour = "#0000FF";
       // rotate towards center
-      this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      // this.acceleration.angular += (this.velocity.theta - this.position.vectorTo(screenCenter).theta)
+      this.acceleration.x = screenCenter.x - this.position.x;
+      this.acceleration.y = screenCenter.y - this.position.y;
     }
+  }
+
+  update() {
+    this.colour = "#8C5523";
+    this.avoidEdges();
 
     let dx = mouse.x - this.x;
     let dy = mouse.y - this.y;
@@ -91,15 +99,23 @@ class Particle {
       }
     }
 
-    // Limit velocity
 
-    
 
     // limit turn rate
+    let oldTheta = this.velocity.theta;
+    this.velocity.x += this.acceleration.x;
+    this.velocity.y += this.acceleration.y;
+    let deltaTheta = 180 - ((180 - this.velocity.theta + oldTheta) % 360);
+    if (deltaTheta > this.maxVelocity.angular) {
+      if (deltaTheta > 0) {
+        this.velocity.theta = oldTheta + this.maxVelocity;
+      } else {
+        this.velocity.theta = oldTheta - this.maxVelocity;
+      }
+    }
+    this.velocity.r = clamp(this.velocity.r,0,this.maxVelocity.linear)
 
-    clamp(this.acceleration.angular, -5,5);
     // move
-    this.velocity.theta += this.acceleration.angular;
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     this.draw();
@@ -112,8 +128,11 @@ function init() {
   let numberOfParticles = (canvas.height * canvas.width) / 9000;
   for (let i = 0; i < numberOfParticles; i++) {
     let size = Math.random() * 5 + 1;
-    let position = new Vector2({ x: screenCenter.x, y: screenCenter.y/2 }); //new Vector2({x: Math.random() * (innerWidth - buffer - buffer) + buffer,y:Math.random() * (innerHeight - buffer - buffer) + buffer})
-    let velocity = new Vector2({x: Math.random() * 6 - 3,y: Math.random() * 6 - 3});
+    let position = new Vector2({ x: screenCenter.x, y: screenCenter.y / 2 }); //new Vector2({x: Math.random() * (innerWidth - buffer - buffer) + buffer,y:Math.random() * (innerHeight - buffer - buffer) + buffer})
+    let velocity = new Vector2({
+      x: Math.random() * 6 - 3,
+      y: Math.random() * 6 - 3,
+    });
     let colour = "#8C5523";
 
     particlesArray.push(new Particle(position, velocity, size, colour));
