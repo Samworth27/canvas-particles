8d95b49e4e6edb0956d0321dbbe77c7e2bc92010
Sam Mitchell <samworth27@gmail.com>
2022-06-30 00:01:59 +1000

    added more examples

--- emitter.js
+++ emitter.js
@@ -13,7 +13,9 @@ function animate(canvas, tick = 15) {
   if (FPSHistory.length > 60) {
     FPSHistory.splice(0, 1);
   }
-  window.fps = Math.round(FPSHistory.reduce((a, b) => a + b) / FPSHistory.length);
+  window.fps = Math.round(
+    FPSHistory.reduce((a, b) => a + b) / FPSHistory.length
+  );
   canvas.draw(dt);
   previousTick = tick;
   requestAnimationFrame((tick) => animate(canvas, tick));
@@ -21,40 +23,76 @@ function animate(canvas, tick = 15) {
 
 const canvas = createCanvas();
 
-let particle = new ParticleDescriptor(
+let fibParticle = new ParticleDescriptor(
   10,
   new HSLA(90, 50, 50, 1),
   "circle",
   20,
   "normal"
 );
-let particle1 = new ParticleDescriptor(
-  10,
-  new HSLA(90, 50, 50, 1),
-  "circle",
-  20,
-  "sin"
-);
-let particle2 = new ParticleDescriptor(
-  10,
-  new HSLA(270, 50, 50, 1),
-  "circle",
-  20,
-  "alt-sin"
-);
 
-let particles = [particle1, particle2];
-
-let options = {
-  direction: "spin",
+let fibOptions = {
+  directionMode: "spin",
   spinSpeed: 3400,
-  selectionMode: "random",
   linkMode: "all",
-  // colourMode: "cycle",
-  // colourCycleSpeed: 360
 };
-let emitter = new Emitter(canvas, particle, new Vector2(200, 300), 84, 0.5,options);
 
-canvas.emitters.push(emitter);
+let fibEmitter = new Emitter(
+  canvas,
+  fibParticle,
+  new Vector2(200, 300),
+  84,
+  0.5,
+  fibOptions
+);
+
+
+
+let oppositeSinParticles = [
+  new ParticleDescriptor(10, new HSLA(0, 50, 50, 1), "circle", 15, "alt-sin"),
+  new ParticleDescriptor(10, new HSLA(180, 50, 50, 1), "circle", 15, "sin"),
+];
+
+let oppositeSinOptions = {
+  directionMode: 'set',
+  direction: 90,
+  selectionMode: 'sequential',
+  linkMode: 'alternate',
+}
+
+let oppositeSinEmitter = new Emitter(
+  canvas,
+  oppositeSinParticles,
+  new Vector2(innerWidth/2, 0),
+  20,
+  1,
+  oppositeSinOptions
+);
+
+let lastParticles = [
+  new ParticleDescriptor(10, new HSLA(0, 50, 100, 1), "circle", 15, "sin"),
+  new ParticleDescriptor(10, new HSLA(270, 50, 50, 1), "circle", 15, "sin-alt"),
+  new ParticleDescriptor(10, new HSLA(270, 50, 50, 1), "circle", 15, "sin-alt"),
+];
+
+let lastOptions = {
+  directionMode: 'spin',
+  spinSpeed: 360,
+  selectionMode: 'sequential',
+  linkMode: 'all'
+}
+
+let lastEmitter = new Emitter(
+  canvas,
+  lastParticles,
+  new Vector2(innerWidth-300, innerHeight/2),
+  20,
+  1,
+  lastOptions
+);
+
+canvas.emitters.push(fibEmitter);
+canvas.emitters.push(oppositeSinEmitter);
+canvas.emitters.push(lastEmitter);
 
 animate(canvas);
