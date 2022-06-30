995678d5c55ee4f466a70049e536426dc38de032
Sam Mitchell <samworth27@gmail.com>
2022-06-30 10:39:33 +1000

    + Physics Particle exports

--- modules/Emitter.js
+++ modules/Emitter.js
@@ -1,3 +1,4 @@
 export {Particle} from './Emitter/Particle.js';
-export {ParticleDescriptor} from './Emitter/ParticleDescriptor.js';
+export {PhysicsParticle} from './Emitter/PhysicsParticle.js';
+export {ParticleDescriptor, PhysicsParticleDescriptor} from './Emitter/ParticleDescriptor.js';
 export {Emitter} from './Emitter/Emitter.js';
